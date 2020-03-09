3d61dae05772410144c1e2c6d851d505a944b92f
OmarPopalzai <61887673+OmarPopalzai@users.noreply.github.com>
2020-03-09 00:31:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-var data = null;
+const app = document.getElementById('root')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+app.appendChild(container)
+
+var data = null; 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=Houston"); 
-xhr.setRequestHeader("Authorization", "Bearer CZCIY3AdgHRjF6VM7Xcpx7RiwMpHn0XnIfxhmQDU5GCh3paMSJAUjSSj09mrkFbdRe11JR3jBxZgMgmlFK_Ozyd68YdLZb7kOaYq9e2DYHZ-Gq-vXPMw8uVJn01kXnYx");
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-    data = JSON.parse(this.responseText);
-    data.forEach(businesses => {
-    const card = document.createElement('div')
-    card.setAttribute('class', 'card')
-    }
-    console.log(data);
+
+xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=New York", true); 
+xhr.setRequestHeader("Authorization", "Bearer lNsIRYPGSFJMX8oD93nl4hD89iueKMgv12lLEr_Jbb_ei0KjFOIwYJJKWtnqwXCWr-et9s3arwLq1VQzZnXoU7tvVf1Y2GhDsgh6vBMBFgvKRelL9Urfblatig1gXnYx");
+
+xhr.onload = function() {
+    var data = JSON.parse(this.response)
+    if (xhr.status >= 200 && xhr.status < 400) {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+      const h1 = document.createElement('h1')
+      h1.textContent = console.log(data.businesses[0].name)
+      console.log(data.businesses[1].name);
+      console.log(data.businesses[2].name);
+      console.log(data.businesses[3].name);
+      console.log(data.businesses[4].name);
+      console.log(data.businesses[5].name);
+      container.appendChild(card)
+      card.appendChild(h1)
+     }
+ else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Not working!`
+    app.appendChild(errorMessage)
   }
-});
-xhr.send(data);
+}
+xhr.send()
